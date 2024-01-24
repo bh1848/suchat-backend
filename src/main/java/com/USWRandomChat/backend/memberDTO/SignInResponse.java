a687472a8fd3fac268e07c8eac5ba9162c9ae7f0
gioJioh <sjgio0515@naver.com>
2024-01-24 23:59:10 +0900

    refactor: siginIn DTO

--- src/main/java/com/USWRandomChat/backend/memberDTO/SignInResponse.java
+++ src/main/java/com/USWRandomChat/backend/memberDTO/SignInResponse.java
@@ -1,6 +1,5 @@
 package com.USWRandomChat.backend.memberDTO;
 
-import com.USWRandomChat.backend.domain.Authority;
 import com.USWRandomChat.backend.domain.Member;
 import com.USWRandomChat.backend.security.jwt.Jwt;
 import com.USWRandomChat.backend.security.jwt.JwtDto;
@@ -10,9 +9,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @Builder
 @AllArgsConstructor
@@ -20,12 +16,12 @@ import java.util.List;
 public class SignInResponse {
 
     private String memberId;
-    private String memberPw;
+    private String password;
     private JwtDto token;
 
     public SignInResponse(Member member, JwtProvider jwtProvider, Jwt jwt) {
         this.memberId = member.getMemberId();
-        this.memberPw = member.getPassword();
+        this.password = member.getPassword();
         this.token = JwtDto.builder()
                 .access_token(jwtProvider.createAccessToken(member.getMemberId(), member.getRoles()))
                 .refresh_token(jwt.getRefreshToken())
