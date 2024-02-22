d8cbe2a63ea24feb8bef987649595fdf04d0c8c5
boogoolboogool <valent9@naver.com>
2024-02-22 23:03:51 +0900

    필요없는 class 삭제

--- src/main/java/com/USWRandomChat/backend/chat/repository/MatchParticipantsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.USWRandomChat.backend.chat.repository;
-
-import com.USWRandomChat.backend.chat.domain.MatchParticipants;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatchParticipantsRepository extends JpaRepository<MatchParticipants, Long> {
-
-}
