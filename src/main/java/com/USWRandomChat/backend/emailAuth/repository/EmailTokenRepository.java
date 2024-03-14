925531376a75b0150cef325280d75cfce0a37181
namgoong <cookie_31@naver.com>
2024-03-14 20:23:10 +0900

    fix: 이메일 인증 확인 조건문 삭제

--- src/main/java/com/USWRandomChat/backend/emailAuth/repository/EmailTokenRepository.java
+++ src/main/java/com/USWRandomChat/backend/emailAuth/repository/EmailTokenRepository.java
@@ -1,7 +1,6 @@
 package com.USWRandomChat.backend.emailAuth.repository;
 
 import com.USWRandomChat.backend.emailAuth.domain.EmailToken;
-import com.USWRandomChat.backend.member.domain.MemberTemp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -14,8 +13,4 @@ public interface EmailTokenRepository extends JpaRepository<EmailToken, String>{
     Optional<EmailToken> findByUuid(String uuid);
     List<EmailToken> findByExpirationDateBeforeAndExpiredIsFalse(LocalDateTime expirationTime);
     void deleteByUuid(String uuid);
-    
-    //member_id로 이메일 토큰 삭제하기 위해 필요
-//    EmailToken findByMember(MemberTemp memberTemp);
-
 }
